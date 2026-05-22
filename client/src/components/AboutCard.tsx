ac37198d987e58065b101e7ddb22ef9e6388f7c3
Sephora Jean <sephorejean@gmail.com>
2026-05-21 22:49:18 -0400

    Initial Commit

--- client/src/components/AboutCard.tsx
+++ client/src/components/AboutCard.tsx
@@ -1,11 +1,12 @@
-function AboutCard(title) {
+function AboutCard() {
     return (
-
+{/*
         <article className="aboutCard">
             <h3>{$title}</h3>
         </article>
+    
+*/}
     )
-
 }
 
 export default AboutCard;
